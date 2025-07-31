1a0ea81c47807934eab6fff45cb6bafedc7c4fa2
zooeywm <zooeywm@gmail.com>
2025-08-01 02:24:19 +0800

    Reinit

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
 mod arrays {
-    mod get_nums_of_sub_array_sum_to_k;
+  mod get_nums_of_sub_array_sum_to_k;
 }
 
 #[cfg(test)]
-mod trees{
-    mod add_one_row_to_tree;
+mod trees {
+  mod add_one_row_to_tree;
 }
